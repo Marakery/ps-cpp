673a7b05d2527c0d7327b7ff993a0899245c435b
Marcus <stfuftlok@gmail.com>
2017-09-19 21:31:40 +0300

    1st lw

---
+++ cpp.1/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int WinMain()
+{
+    cout << "Hola, Marcus!" << endl;
+}
---
+++ cpp.1/hello_err.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int WinMain()
+{
+    std::cuout << "Hola, Marcus!" << std::endl;
+}
