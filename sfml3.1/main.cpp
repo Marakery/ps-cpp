f1333296796e420b7c3df73985c0508ab21f0587
Marcus <marcus.koh16.01.97@gmail.com>
2018-01-08 16:40:17 +0300

     

--- sfml3.1/main.cpp
+++ sfml3.1/main.cpp
@@ -4,63 +4,106 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+struct Eye1
+{
+	sf::ConvexShape whiteEye;
+	sf::ConvexShape appleEye;
+
+
+
+	sf::Vector2f position;
+
+	float rotation = 0;
+};
+/*struct Eye2
+{
+	sf::ConvexShape whiteEye2;
+	sf::ConvexShape appleEye2;
+
+
+	sf::Vector2f position;
+
+	float rotation = 0;
+};*/
+
+
+sf::Vector2f toEuclidian(float radiusX, float radiusY, float angle)
+{
+	return{
+		radiusX * float(cos(angle)),
+		radiusY * float(sin(angle)) };
+}
+
+float toGrees(float radians) { return float(double(radians) * 180.0 / M_PI); }
+sf::Vector2f relativePos(sf::Vector2f posRoot, sf::Vector2f absolPos) {
+	return absolPos - posRoot;
+}
+
+void updateEyesElements(Eye1 &eye1/*, Eye2 &eye2*/)
+{
+	constexpr float rotationRadiusX = 50.f;
+	constexpr float rotationRadiusY = 100.f;
+
+	const sf::Vector2f whiteOfTheEye1Offset = toEuclidian(rotationRadiusX, rotationRadiusY, eye1.rotation);
+	eye1.appleEye.setPosition(eye1.position + whiteOfTheEye1Offset);
+
+
+
+
+}
+
+void initEyes(Eye1& eye1)
+{
+	const sf::Vector2f whiteRadius = { 80.f, 150.f };
+	const sf::Vector2f appleRadius = { 10.f, 15.f };
+	constexpr int pointCount = 200;
+
+	eye1.position = {200, 200 };
+
+	eye1.whiteEye.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
+	eye1.whiteEye.setPointCount(pointCount);
+	eye1.whiteEye.setPosition(eye1.position);
+
+	eye1.appleEye.setFillColor(sf::Color::Black);
+	eye1.appleEye.setPointCount(pointCount);
+	eye1.appleEye.setPosition(eye1.position);
+	
+	for (int pointNo = 0; pointNo < pointCount; ++pointNo)
+	{
+		float angle = float(2 * M_PI * pointNo) / float(pointCount);
+		sf::Vector2f point = {
+			whiteRadius.x * std::sin(angle),
+			whiteRadius.y * std::cos(angle) };
+
+	
+		eye1.whiteEye.setPoint(pointNo, point);
+
+	}
+
+	
+	for (int pointNo = 0; pointNo < pointCount; ++pointNo)
+	{
+		float angle = float(2 * M_PI * pointNo) / float(pointCount);
+		sf::Vector2f point = {
+			appleRadius.x * std::sin(angle),
+			appleRadius.y * std::cos(angle) };
+
+		eye1.appleEye.setPoint(pointNo, point);
+	}
+
+	//eye1.appleEye.setRadius(15);
+	//eye1.appleEye.setOrigin(sf::Vector2f(0, 15));
+	//eye1.appleEye.setPosition(eye1.position);
+	//eye1.appleEye.setFillColor(sf::Color::Black);
+
+
+	updateEyesElements(eye1);
+}
+
 void onMouseMove(const sf::Event::MouseMoveEvent &event, sf::Vector2f &mousePosition)
 {
 	std::cout << "mouse x=" << event.x << ", y=" << event.y << std::endl;
-
 	mousePosition = { float(event.x), float(event.y) };
-}
-
-void init(sf::ConvexShape& arrow)
-{
-	arrow.setPointCount(3);
-	arrow.setPoint(0, { 40, 0 });
-	arrow.setPoint(1, { -20, -20 });
-	arrow.setPoint(2, { -20, 20 });
-	arrow.setPosition({ 400, 300 });
-	arrow.setFillColor(sf::Color::Blue);
-
-}
-float toDegrees(float radians)
-{
-	return float(double(radians) * 180.0 / M_PI);
-}
-
-void update(const sf::Vector2f& mousePosition, sf::ConvexShape& arrow, float& dt)
-{
-	const sf::Vector2f delta = mousePosition - arrow.getPosition();
-	float angle = atan2(delta.y, delta.x);
-	const float arrowSpeed = 20;
-	if (angle < 0) 
-	{
-		angle = angle + 2 * M_PI;
-	}
-	const float arrowRotation = arrow.getRotation();
-	const float mouseRotation = toDegrees(angle);
-	const float maxRotation = arrowSpeed * dt;
-	float nextRotation = std::min((mouseRotation - maxRotation), maxRotation);
-	if (mouseRotation < arrowRotation)
-	{
-		if ((mouseRotation + 180) < arrowRotation)
-		{
-			arrow.setRotation(arrowRotation + nextRotation);
-		}
-		else
-		{
-			arrow.setRotation(arrowRotation - nextRotation);
-		}
-	}
-	else
-	{
-		if ((mouseRotation - 180) > arrowRotation)
-		{
-			arrow.setRotation(arrowRotation - nextRotation);
-		}
-		else
-		{
-			arrow.setRotation(arrowRotation + nextRotation);
-		}
-	}
 }
 
 void pollEvents(sf::RenderWindow &window, sf::Vector2f &mousePosition)
@@ -82,31 +125,45 @@ void pollEvents(sf::RenderWindow &window, sf::Vector2f &mousePosition)
 	}
 }
 
-void redrawFrame(sf::RenderWindow& window, sf::ConvexShape& arrow)
+void updateEyes(const sf::Vector2f &mousePosition, Eye1 &eye1)
+{
+
+	const sf::Vector2f firstEyeDelta = mousePosition - eye1.position;
+	eye1.rotation = atan2(firstEyeDelta.y, firstEyeDelta.x);
+
+
+	updateEyesElements(eye1);
+}
+
+void redrawFrame(sf::RenderWindow &window, Eye1 &eye1)
 {
 	window.clear();
-	window.draw(arrow);
+	window.draw(eye1.whiteEye);
+	window.draw(eye1.appleEye);
 	window.display();
 }
 
-
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(900, 600), "Arrow follow mouse");
+	constexpr unsigned WINDOW_WIDTH = 800;
+	constexpr unsigned WINDOW_HEIGHT = 600;
 
-	sf::ConvexShape arrow;
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	sf::RenderWindow window(
+		sf::VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT }),
+		"Eyes follows mouse", sf::Style::Default, settings);
+
+
+	Eye1 eye1;
 	sf::Vector2f mousePosition;
-	sf::Clock clock;
 
-	init(arrow);
+	initEyes(eye1);
 	while (window.isOpen())
 	{
 		pollEvents(window, mousePosition);
-		float dt = clock.restart().asSeconds();
-		update(mousePosition, arrow, dt);
-		redrawFrame(window, arrow);
+		updateEyes(mousePosition, eye1);
+		redrawFrame(window, eye1);
 	}
 }
-
-
 
